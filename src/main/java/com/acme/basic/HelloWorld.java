fcc934a896227ad8878e7898b9fac3dc17e10e1f
rpecor-sonar <123400049+rpecor-sonar@users.noreply.github.com>
2023-05-04 13:37:45 -0500

    PR 3

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -11,3 +11,15 @@ public class HelloWorld {
   }
 
 }
+
+// public class HelloWorld {
+
+//   void sayHello() {
+//     System.out.println("Hello World!");
+//   }
+
+//   void notCovered() {
+//     System.out.println("This method is not covered by unit tests");
+//   }
+
+// }
