b93362bceedde53c1fafb8718219046629b5fb72
Ryan Pecor <123400049+rpecor-sonar@users.noreply.github.com>
2023-05-31 12:11:44 -0500

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -9,5 +9,20 @@ public class HelloWorld {
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
+  
+  void sayHello2() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered2() {
+    System.out.println("This method is not covered by unit tests");
+  }
+   void sayHello3() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered3() {
+    System.out.println("This method is not covered by unit tests");
+  }
 
 }
